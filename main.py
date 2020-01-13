5c7e5005be6d13820e7a9311cbbbed0c2c79ba65
Bhawesh Bhansali <bhansalibhawesh@yahoo.com>
2020-01-13 17:29:44 +0530

    feat: Implement parser

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ TT_INT		= 'INT'
 TT_STR      = 'STR'
 TT_LPAREN   = 'LPAREN'
 TT_RPAREN   = 'RPAREN'
-TT_COMMA    = ','
+TT_COMMA    = 'COMMA'
 DIGITS = '0123456789'  # for positions and page numbers
 ALPHABETS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_.'  # for image file name
 
@@ -40,7 +40,7 @@ class Lexer:
     def advance(self):
         self.pos.advance()
         self.curr_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
-    
+
     ##
     # method to tokenize into chunks
     def make_tokens(self):
@@ -69,7 +69,7 @@ class Lexer:
                 self.advance()
         multiline_tokens.append(tokens)
         return multiline_tokens
-    
+
     ##
     # method to tokenize the value of integers
     def make_number(self):
@@ -78,7 +78,7 @@ class Lexer:
             num_str += self.curr_char
             self.advance()
         return Token(TT_INT, int(num_str))
-    
+
     ##
     # method to tokenize the value of strings
     def make_string(self):
@@ -88,6 +88,68 @@ class Lexer:
             self.advance()
         return Token(TT_STR, image_file_str)
 
+
+class FlipBook:
+    def __init__(self, init_page, end_page, image_file, x_pos, y_pos):
+        self.init_page = init_page
+        self.end_page = end_page
+        self.image_file = image_file
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+
+class ElementNode:
+    def __init__(self, tok):
+        self.tok = tok
+
+    def __repr__(self):
+        return str(self.tok)
+
+##
+# method to Parse the tokens
+class Parser:
+    def __init__(self, list_of_tokens):
+        self.list_of_tokens = list_of_tokens
+        self.list_idx = 0
+        self.tok_idx = -1
+        self.advance()
+
+    def advance(self):
+        self.tok_idx += 1
+        if self.tok_idx < len(self.list_of_tokens[self.list_idx]):
+            self.curr_tok = self.list_of_tokens[self.list_idx][self.tok_idx]
+        elif self.list_idx < (len(self.list_of_tokens)-1):
+            self.tok_idx = 0
+            self.list_idx += 1
+            self.curr_tok = self.list_of_tokens[self.list_idx][self.tok_idx]
+        return self.curr_tok
+
+    def factor_number(self):
+        tok = self.curr_tok
+        if tok.type ==  TT_INT:
+            self.advance()
+            return ElementNode(tok)
+
+    def factor_string(self):
+        tok = self.curr_tok
+        if tok.type == TT_STR:
+            self.advance()
+            return ElementNode(tok)
+
+    def parse(self):
+        while self.curr_tok.value != None:
+            init_page = self.factor_number()
+            end_page = self.factor_number()
+            image_file = self.factor_string()
+            position_string = self.factor_string()
+
+            if self.curr_tok.type == TT_LPAREN:
+                self.advance()
+                x_pos = self.factor_number()
+                self.advance()  # skip comma
+                y_pos = self.factor_number()
+                self.advance()  # RPAREN
+            print FlipBook(init_page, end_page, image_file, x_pos, y_pos).__dict__
+
 ##
 # the main driver function
 def main():
@@ -95,6 +157,8 @@ def main():
         text = f.read()
     lexer = Lexer(text)  # the lexer object
     tokens = lexer.make_tokens()  # generate the tokens
-    print tokens
+    parser = Parser(tokens)  # parse the tokens
+    ast = parser.parse()
+    # print ast.__dict__
 
 main()
